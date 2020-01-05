7f65c2cb7e02546a3942cccbbf8d526ccadc2050
guangtuan <zhuangguangtuan@gmail.com>
2020-01-05 21:07:07 +0800

    refactor: 明细编辑接口修改，增加对明细是否存在于数据库的判断

--- src/main/kotlin/tech/igrant/jizhang/detail/Detail.kt
+++ src/main/kotlin/tech/igrant/jizhang/detail/Detail.kt
@@ -1,5 +1,7 @@
 package tech.igrant.jizhang.detail
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -17,6 +19,24 @@ class Detail(
         var updatedAt: Date?,
         var amount: Int,
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
+)
+
+@ApiModel("明细更新所传的对象")
+class DetailUpdateTo(
+        @ApiModelProperty("用户id")
+        val userId: Long,
+        @ApiModelProperty("来源账户id，如用支付宝吃饭，来源账户就是支付宝")
+        val sourceAccountId: Long? = null,
+        @ApiModelProperty("目标账户id，如发工资的时候，目标账户就是工资卡")
+        val destAccountId: Long? = null,
+        @ApiModelProperty("科目id")
+        val subjectId: Long,
+        @ApiModelProperty("备注")
+        val remark: String? = null,
+        @ApiModelProperty("金额，单位分")
+        val amount: Int? = null,
+        @ApiModelProperty("需要更新的明细id")
+        val id: Long
 )
 
 data class DetailVo(
--- src/main/kotlin/tech/igrant/jizhang/detail/DetailController.kt
+++ src/main/kotlin/tech/igrant/jizhang/detail/DetailController.kt
@@ -1,8 +1,10 @@
 package tech.igrant.jizhang.detail
 
 import io.swagger.annotations.ApiOperation
+import org.springframework.beans.BeanUtils
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import tech.igrant.jizhang.account.AccountRepo
 import tech.igrant.jizhang.subject.SubjectRepo
@@ -49,20 +51,27 @@ class DetailController(
 
     @ApiOperation("更新一条明细")
     @PutMapping
-    fun update(@RequestBody detail: Detail): DetailVo {
-        val user = userRepo.findById(detail.userId).get()
-        val subject = subjectRepo.findById(detail.subjectId).get()
-        val accountNameMap = accountRepo.findAllById(
-                listOf(detail.sourceAccountId, detail.destAccountId)
-        ).associateBy({ a -> a.id }, { a -> a.name })
-        detailRepo.save(detail)
-        return DetailVo.fromPo(
-                detail,
-                username = user.username,
-                sourceAccountName = accountNameMap[detail.sourceAccountId].orEmpty(),
-                destAccountName = accountNameMap[detail.destAccountId].orEmpty(),
-                subjectName = subject.name
-        )
+    @ResponseBody
+    fun update(@RequestBody detail: DetailUpdateTo): ResponseEntity<DetailVo> {
+        val detailInDbOpt = detailRepo.findById(detail.id)
+        if (detailInDbOpt.isPresent) {
+            val detailInDb = detailInDbOpt.get()
+            val user = userRepo.findById(detail.userId).get()
+            val subject = subjectRepo.findById(detail.subjectId).get()
+            val accountNameMap = accountRepo.findAllById(
+                    listOf(detail.sourceAccountId, detail.destAccountId)
+            ).associateBy({ a -> a.id }, { a -> a.name })
+            BeanUtils.copyProperties(detail, detailInDb)
+            detailRepo.save(detailInDb)
+            return ResponseEntity.ok(DetailVo.fromPo(
+                    detailInDb,
+                    username = user.username,
+                    sourceAccountName = accountNameMap[detail.sourceAccountId].orEmpty(),
+                    destAccountName = accountNameMap[detail.destAccountId].orEmpty(),
+                    subjectName = subject.name
+            ))
+        } else {
+            return ResponseEntity.notFound().build()
+        }
     }
-
 }
