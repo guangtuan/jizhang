86eb6234efecd4c3c240c36a0b3b79452d2905c4
guangtuan <zhuangguangtuan@gmail.com>
2020-09-20 18:17:17 +0800

    fix: add default impl of FixedExpenseService to fix error: no Component of type: FixedExpenseService

--- src/main/kotlin/tech/igrant/jizhang/fix/FixedExpenseService.kt
+++ src/main/kotlin/tech/igrant/jizhang/fix/FixedExpenseService.kt
@@ -1,9 +1,24 @@
 package tech.igrant.jizhang.fix
 
+import org.springframework.stereotype.Component
+
 interface FixedExpenseService {
 
     fun create(fixedExpenseTo: FixedExpenseTo): FixedExpenseVo
 
     fun generate(id: Long): Void
+
+    @Component
+    class Impl : FixedExpenseService {
+
+        override fun create(fixedExpenseTo: FixedExpenseTo): FixedExpenseVo {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun generate(id: Long): Void {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
 
 }
