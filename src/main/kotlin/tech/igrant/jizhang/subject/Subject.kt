dfc09b63083c82243d0d12285b72cd72524b8da5
guangtuan <zhuangguangtuan@gmail.com>
2020-10-11 23:09:54 +0800

    refactor: parent and child mode for subject

--- src/main/kotlin/tech/igrant/jizhang/subject/Subject.kt
+++ src/main/kotlin/tech/igrant/jizhang/subject/Subject.kt
@@ -10,5 +10,41 @@ class Subject (
         var name: String,
         var description: String,
         var tags: String,
+        val parentId: Long? = null,
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
+) {
+    fun toVo(): SubjectVo {
+        return SubjectVo(
+                name = this.name,
+                description = this.description,
+                id = this.id!!,
+                children = mutableListOf(),
+                parentId = this.parentId
+        )
+    }
+}
+
+class SubjectVo (
+        val name: String,
+        val description: String,
+        val id: Long,
+        val children: MutableList<SubjectVo>,
+        val parentId: Long?
 )
+
+class SubjectTo (
+        private val name: String,
+        private val tags: String,
+        private val description: String,
+        private val parentId: Long
+) {
+    fun toPo(): Subject {
+        return Subject(
+                name = name,
+                description = description,
+                parentId = parentId,
+                tags = tags,
+                id = null
+        )
+    }
+}
--- src/main/kotlin/tech/igrant/jizhang/subject/SubjectController.kt
+++ src/main/kotlin/tech/igrant/jizhang/subject/SubjectController.kt
@@ -1,25 +1,42 @@
 package tech.igrant.jizhang.subject
 
 import io.swagger.annotations.ApiOperation
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/subjects")
 @ApiOperation("科目接口")
-class SubjectController(private val subjectRepo: SubjectRepo) {
+class SubjectController(
+        private val subjectRepo: SubjectRepo
+) {
 
     @ApiOperation("列出科目")
     @GetMapping()
-    fun list(): List<Subject> {
+    fun list(): List<SubjectVo> {
         return subjectRepo.findAll().toList()
+                .fold(
+                        mutableMapOf<Long, SubjectVo>(),
+                        { acc, po ->
+                            po.parentId?.let {
+                                acc[it]?.children?.add(po.toVo())
+                            }
+                            if (!acc.containsKey(po.id)) {
+                                po.toVo().apply {
+                                    acc[this.id] = this
+                                }
+                            }
+                            acc
+                        }
+                )
+                .values
+                .filter { s -> s.parentId == null }
+                .toList()
     }
 
     @ApiOperation("新建一个科目")
     @PostMapping
-    fun create(@RequestBody subject: Subject): Subject {
-        return subjectRepo.save(subject)
+    fun create(@RequestBody subjectTo: SubjectTo): Subject {
+        return subjectRepo.save(subjectTo.toPo())
     }
 
 }
