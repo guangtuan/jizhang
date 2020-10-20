6add29ef0f52e4fd458d851948c97d6566e04fa0
guangtuan <zhuangguangtuan@gmail.com>
2020-10-20 22:19:45 +0800

    refactor: display subjects by level

--- src/main/kotlin/tech/igrant/jizhang/subject/SubjectController.kt
+++ src/main/kotlin/tech/igrant/jizhang/subject/SubjectController.kt
@@ -1,6 +1,7 @@
 package tech.igrant.jizhang.subject
 
 import io.swagger.annotations.ApiOperation
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -23,6 +24,17 @@ class SubjectController(
             }
         }
         return list
+    }
+
+    @ApiOperation("根据登记拿到科目")
+    @GetMapping(params = ["by=level"])
+    fun listByLevel(@RequestParam("level", required = true) level: Int, @RequestParam("parentId", required = false) parentId: Long?): ResponseEntity<List<SubjectVo>> {
+        return when (level) {
+            1 -> ResponseEntity.ok(subjectRepo.findParent().map { po -> po.toVo() }.toList())
+            2 -> parentId?.let { ResponseEntity.ok(subjectRepo.findChildrenByParent(it).map { po -> po.toVo() }.toList()) }
+                    ?: ResponseEntity.ok(subjectRepo.findChildren().map { po -> po.toVo() }.toList())
+            else -> ResponseEntity.badRequest().build()
+        }
     }
 
     @ApiOperation("新建一个科目")
--- src/main/kotlin/tech/igrant/jizhang/subject/SubjectRepo.kt
+++ src/main/kotlin/tech/igrant/jizhang/subject/SubjectRepo.kt
@@ -2,6 +2,7 @@ package tech.igrant.jizhang.subject
 
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
 interface SubjectRepo : CrudRepository<Subject, Long> {
 
@@ -10,5 +11,8 @@ interface SubjectRepo : CrudRepository<Subject, Long> {
 
     @Query(nativeQuery = true, value = "select * from subject where level = 2")
     fun findChildren(): List<Subject>
+
+    @Query(nativeQuery = true, value = "select * from subject where level = 2 and parent_id = :parentId")
+    fun findChildrenByParent(@Param("parentId") parentId: Long): List<Subject>
 
 }
