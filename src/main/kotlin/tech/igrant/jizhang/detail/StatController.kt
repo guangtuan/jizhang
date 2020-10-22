c6e63edb4d43e7300470b3f72f0bc6c8bf972d6d
guangtuan <zhuangguangtuan@gmail.com>
2020-10-22 22:04:02 +0800

    feat: default to all subjectIds on statistics

--- src/main/kotlin/tech/igrant/jizhang/detail/StatController.kt
+++ src/main/kotlin/tech/igrant/jizhang/detail/StatController.kt
@@ -5,18 +5,31 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import tech.igrant.jizhang.ext.getStartOfTomorrow
+import tech.igrant.jizhang.subject.SubjectRepo
 
 @RestController
 @RequestMapping("/api/stats")
-class StatController(private val detailRepo: DetailRepo) {
+class StatController(
+        private val detailRepo: DetailRepo,
+        private val subjectRepo: SubjectRepo
+) {
 
     @PostMapping()
     fun query(@RequestBody amountTotalQuery: AmountTotalQuery): List<AmountTotal> {
-        return detailRepo.query(
-                amountTotalQuery.start,
-                amountTotalQuery.end.getStartOfTomorrow(),
-                amountTotalQuery.subjects
-        )
+        return if (amountTotalQuery.subjects.isEmpty()) {
+            detailRepo.query(
+                    amountTotalQuery.start,
+                    amountTotalQuery.end.getStartOfTomorrow(),
+                    subjectIds = subjectRepo.findAll().mapNotNull { sub -> sub.id }
+            )
+        } else {
+            detailRepo.query(
+                    amountTotalQuery.start,
+                    amountTotalQuery.end.getStartOfTomorrow(),
+                    amountTotalQuery.subjects
+            )
+        }
+
     }
 
 }
